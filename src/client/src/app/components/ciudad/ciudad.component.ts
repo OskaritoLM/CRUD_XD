5b5286bf687a900882f53fdec8c00c5277c0fa80
Brayanpower <116902837+Brayanpower@users.noreply.github.com>
2024-03-13 11:06:31 -0600

    ciudad p1

--- src/client/src/app/components/ciudad/ciudad.component.ts
+++ src/client/src/app/components/ciudad/ciudad.component.ts
@@ -1,7 +1,9 @@
 // ciudad.component.ts
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CiudadService } from '../../services/ciudad.service';
 import { DatosPService } from '../../services/datos-pservice.service';
+import { CuidadModel } from '../../models/datosPModel';
 import { DatosPModel } from '../../models/datosPModel';
 
 @Component({
@@ -10,29 +12,94 @@ import { DatosPModel } from '../../models/datosPModel';
   styleUrls: ['./ciudad.component.css']
 })
 export class CiudadComponent implements OnInit {
-  datosPaises: DatosPModel[] = [];
-  paisForm: FormGroup;
+  ciudades: CuidadModel[] = [];
+  paises: DatosPModel[] = [];
+  datosPaises: DatosPModel[] = []; // Agregar esta línea
+  ciudadForm: FormGroup;
 
-  constructor(private paisService: DatosPService, private fb: FormBuilder) {
-    this.paisForm = this.fb.group({
+  constructor(
+    private ciudadService: CiudadService,
+    private paisService: DatosPService,
+    private fb: FormBuilder
+  ) {
+    this.ciudadForm = this.fb.group({
       _id: [''],
-      nombre: ['', Validators.required]
+      nombre: ['', Validators.required],
+      pais: ['', Validators.required]
     });
   }
 
   ngOnInit() {
-    this.cargarDatosP();
+    this.cargarCiudades();
+    this.cargarPaises();
   }
 
-  cargarDatosP() {
+  cargarCiudades() {
+    this.ciudadService.getDatosP().subscribe(
+      data => {
+        this.ciudades = data;
+        console.log('Ciudades cargadas:', this.ciudades);
+      },
+      error => {
+        console.error('Error al cargar ciudades:', error);
+      }
+    );
+  }
+
+  cargarPaises() {
     this.paisService.getDatosP().subscribe(
       data => {
-        this.datosPaises = data;
+        this.datosPaises = data; // Actualizar la propiedad datosPaises
         console.log('Datos de países cargados:', this.datosPaises);
       },
       error => {
         console.error('Error al cargar datos de países:', error);
       }
     );
+  }
+
+  crearCiudad(event: Event) {
+    event.preventDefault();
+  
+    if (this.ciudadForm.valid) {
+      const nuevaCiudad: CuidadModel = {
+        nombre: this.ciudadForm.value.nombre,
+        pais: this.ciudadForm.value.pais,
+      };
+  
+      this.ciudadService.addDatosP(nuevaCiudad).subscribe(
+        data => {
+          console.log('Ciudad creada:', data);
+          this.ciudadForm.reset();
+          this.cargarCiudades();
+        },
+        error => {
+          console.error('Error al crear ciudad:', error);
+        }
+      );
+    } else {
+      console.error('Formulario no válido. Por favor, complete todos los campos requeridos.');
+    }
+  }
+  
+
+  editarCiudad(ciudad: CuidadModel) {
+    // Implement the logic for editing a city
+    // You can pre-fill the form with the selected city's data
+    // For example: this.ciudadForm.setValue(ciudad);
+  }
+
+  eliminarCiudad(id: string | undefined) {
+    if (id) {
+      this.ciudadService.deleteDatosP(id).subscribe(
+        data => {
+          console.log('Ciudad eliminada:', data);
+          this.cargarCiudades();
+        },
+        error => {
+          console.error('Error al eliminar ciudad:', error);
+        }
+      );
+    }
   }
 }
--- src/client/src/app/models/datosPModel.ts
+++ src/client/src/app/models/datosPModel.ts
@@ -2,3 +2,8 @@ export class DatosPModel {
     _id?: string;
     nombre: string="";
 }
+export class CuidadModel {
+    _id?: string;
+    nombre: string="";
+    pais: string="";
+}
