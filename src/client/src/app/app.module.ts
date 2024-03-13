d9159df6a4cd558a849965c9bcee035efad9a57f
Brayanpower <116902837+Brayanpower@users.noreply.github.com>
2024-03-13 10:26:38 -0600

    p2

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { DatosPService } from './services/datos-pservice.service';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {  ReactiveFormsModule } from '@angular/forms';
 
 import { RegisterComponent } from './components/register/register.component';
 import { PaisComponent } from './components/pais/pais.component';
+import { CiudadComponent } from './components/ciudad/ciudad.component';
 @NgModule({
   declarations: [
     AppComponent,
-
+    CiudadComponent,
     RegisterComponent,
     PaisComponent
   ],
--- src/client/src/app/components/ciudad/ciudad.component.ts
+++ src/client/src/app/components/ciudad/ciudad.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+// ciudad.component.ts
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DatosPService } from '../../services/datos-pservice.service';
+import { DatosPModel } from '../../models/datosPModel';
 
 @Component({
   selector: 'app-ciudad',
   templateUrl: './ciudad.component.html',
   styleUrls: ['./ciudad.component.css']
 })
-export class CiudadComponent {
+export class CiudadComponent implements OnInit {
+  datosPaises: DatosPModel[] = [];
+  paisForm: FormGroup;
 
+  constructor(private paisService: DatosPService, private fb: FormBuilder) {
+    this.paisForm = this.fb.group({
+      _id: [''],
+      nombre: ['', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+    this.cargarDatosP();
+  }
+
+  cargarDatosP() {
+    this.paisService.getDatosP().subscribe(
+      data => {
+        this.datosPaises = data;
+        console.log('Datos de países cargados:', this.datosPaises);
+      },
+      error => {
+        console.error('Error al cargar datos de países:', error);
+      }
+    );
+  }
 }
