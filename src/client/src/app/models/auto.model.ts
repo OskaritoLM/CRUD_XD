1ba33e54bfd8cfa90e8127efbb2cd08d5ad4f498
Darka <darkangel13gamer@gmail.com>
2024-03-14 10:32:49 -0600

    Create auto.model.ts

---
+++ src/client/src/app/models/auto.model.ts
@@ -0,0 +1,10 @@
+// auto.model.ts
+export interface Auto {
+    _id: string;
+    imagen: String;
+    marca: string;
+    modelo: string;
+    año: number;
+    // Otros campos según tu esquema
+  }
+  
